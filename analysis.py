977d8a958e53a1c382cf96c16c052f47db244b8e
flaprimo <fla.primo.engineer@outlook.com>
2018-08-27 19:24:43 +0200

    added quality measures for modularity

--- analysis.py
+++ analysis.py
@@ -1,13 +1,18 @@
 import networkx as nx
 import pandas as pd
 from tqdm import tqdm
+import pquality.PartitionQuality as Pq
+import numpy as np
 import config
+
+pd.set_option('display.max_columns', None)
 
 
 def main():
     g = load_graph()
     c_subgraphs = get_community_subgraphs(g)
 
+    scores = get_pquality(g, c_subgraphs)
     c_hindex_list = iter_metric_c(c_subgraphs, add_hindex, 'hindex')
     c_indegree_list = iter_metric_c(c_subgraphs, add_indegree, 'indegree')
 
@@ -68,6 +73,50 @@ def get_community_subgraphs(g):
           f'{[(c[0], list(c[1].nodes(data=True))[1]) for c in c_subgraphs]}\n\n')
 
     return c_subgraphs
+
+
+def get_pquality(g, g_subgraphs):
+    print('# PARTITION QUALITY')
+
+    n_cut, ied, aid, fomd, ex, cr, cond, nedges, modf, aodf, flake, tpr = ([] for _ in range(12))
+
+    for c_label, community in tqdm(g_subgraphs):
+        n_cut.append(Pq.normalized_cut(g, community))
+        ied.append(Pq.internal_edge_density(community))
+        aid.append(Pq.average_internal_degree(community))
+        fomd.append(Pq.fraction_over_median_degree(community))
+        ex.append(Pq.expansion(g, community))
+        cr.append(Pq.cut_ratio(g, community))
+        nedges.append(Pq.edges_inside(community))
+        cond.append(Pq.conductance(g, community))
+        modf.append(Pq.max_odf(g, community))
+        aodf.append(Pq.avg_odf(g, community))
+        flake.append(Pq.flake_odf(g, community))
+
+    m1 = [
+        ['Internal Density', min(ied), max(ied), np.mean(ied), np.std(ied)],
+        ['Edges inside', min(nedges), max(nedges), np.mean(nedges), np.std(nedges)],
+        ['Average Degree', min(aid), max(aid), np.mean(aid), np.std(aid)],
+        ['FOMD', min(fomd), max(fomd), np.mean(fomd), np.std(fomd)],
+        ['Expansion', min(ex), max(ex), np.mean(ex), np.std(ex)],
+        ['Cut Ratio', min(cr), max(cr), np.mean(cr), np.std(cr)],
+        ['Conductance', min(cond), max(cond), np.mean(cond), np.std(cond)],
+        ['Normalized Cut', min(n_cut), max(n_cut), np.mean(n_cut), np.std(n_cut)],
+        ['Maximum-ODF', min(modf), max(modf), np.mean(modf), np.std(modf)],
+        ['Average-ODF', min(aodf), max(aodf), np.mean(aodf), np.std(aodf)],
+        ['Flake-ODF', min(flake), max(flake), np.mean(flake), np.std(flake)]
+    ]
+    df_scores = pd.DataFrame(m1, columns=['Index', 'min', 'max', 'avg', 'std']).set_index('Index')
+
+    print(f'summary of partition metrics:\n{df_scores.to_string()}\n\n')
+
+    return df_scores
+
+
+def pq_summary(g, g_subgraphs):
+
+
+    return df
 
 
 def add_hindex(g):

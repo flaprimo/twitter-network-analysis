6fc93f92f5652cb816f1742514da13e5f3152d9f
flaprimo <91EL(gi7hu8)barto91>
2018-10-30 19:24:59 -0300

    complete description of each output for community_detection pipeline, started profiling pipeline, introduced persistence with sqlalchemy orm, defined relations and entities to be persisted

--- database/database.py
+++ database/database.py
@@ -4,5 +4,6 @@ from sqlalchemy.orm import sessionmaker
 # engine = create_engine('sqlite:///output/db/db.db')
 engine = create_engine('sqlite:///:memory:', echo=True)
 
+
 def get_session():
     return sessionmaker(bind=engine)

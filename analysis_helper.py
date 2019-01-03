b9f425015afc86b4562ec1f69e818f429adb0d18
flaprimo <91EL(gi7hu8)barto91>
2019-01-03 15:01:13 +0100

    documentation of analysis_helper, improved get_common_nodes with user and profile infos, plot_events_with_common_nodes now sorts the events and prints the number on top of the plot bars

--- analysis_helper.py
+++ analysis_helper.py
@@ -2,24 +2,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datasources import PipelineIO
+from datasources.database.database import db
+from datasources.database.model import User, Profile
 
 
 class AnalysisHelper:
-    # INPUT:
-    # {
-    #     'pipeline_name': ['output1', 'output2'],
-    #     'pipeline_name': ['output1', 'output2']
-    # }
-    #
-    # OUTPUT:
-    # {
-    #     'pipeline_name': {
-    #         'output1': {},
-    #         'output2': {}
-    #     }
-    # }
+    """
+    Helper class that supports analysis results in Jupyter Notebooks.
+    """
+
     @staticmethod
     def load_ds_results(pipeline_outputs, ds_results):
+        """Returns the desired pipeline outputs for a dataset.
+
+        :param pipeline_outputs: dictionary which specifies the desired results names.
+
+        At the first level of the dictionary are the pipeline stage names and
+        at the second level, are a list of the desired output names.
+
+        example:
+        {
+            'pipeline_name': ['output1', ...],
+            ...
+        }
+        :param ds_results: single dataset output format that is returned after a pipeline execution.
+        :return: dictionary with the desired results for a single dataset.
+
+        At the first level of the dictionary are the pipeline stage names and
+        at the second level, are a the desired output names together with the actual results.
+
+        example:
+        {
+            'pipeline_name1': {
+                'output_name1': {},
+                ...
+            },
+            ...
+        }
+        """
         res = {}
         for pipeline_name, output_name_list in pipeline_outputs.items():
             res[pipeline_name] = PipelineIO.load_input(output_name_list, None, ds_results[pipeline_name])
@@ -28,6 +48,38 @@ class AnalysisHelper:
 
     @staticmethod
     def load_all_results(pipeline_outputs, results):
+        """Returns the desired pipeline outputs for all datesets.
+
+        :param pipeline_outputs: dictionary which specifies the desired results names.
+
+        At the first level of the dictionary are the pipeline stage names and
+        at the second level, are a list of the desired output names.
+
+        example:
+        {
+            'pipeline_name1': ['output_name1', ...],
+            ...
+        }
+        :param ds_result
+        :param results: output format that is returned after a pipeline execution.
+        :return: dictionary with the desired results from all datasets.
+
+        At the first level of the dictionary are the dataset names,
+        at the second level are the pipeline stage names and
+        at the third level, are a the desired output names together with the actual results.
+
+        example:
+        {
+            'dataset_name1': {
+                'pipeline_name1': {
+                    'output_name1': {},
+                    ...
+                },
+                ...
+            },
+            ...
+        }
+        """
         res = {}
         for ds_name, ds in results.items():
             res[ds_name] = AnalysisHelper.load_ds_results(pipeline_outputs, ds)
@@ -36,6 +88,14 @@ class AnalysisHelper:
 
     @staticmethod
     def get_single_summary(pipeline_name, output_name, results):
+        """Returns a merged summary of metrics for a desired output result.
+
+        :param pipeline_name: name of the pipeline to which the desired output belongs to.
+        :param output_name: name of the output to which the desired output belongs to.
+        :param results: output format that is returned after a pipeline execution.
+        :return: pandas dataframe.
+        """
+
         # get results of interest
         filtered_results = AnalysisHelper.load_all_results({pipeline_name: [output_name]}, results)
 
@@ -51,6 +111,14 @@ class AnalysisHelper:
 
     @staticmethod
     def get_multi_summary(pipeline_name, output_name, results):
+        """Returns multiple merged summary of metrics for a desired output result.
+
+        :param pipeline_name: name of the pipeline to which the desired output belongs to.
+        :param output_name: name of the output to which the desired output belongs to.
+        :param results: output format that is returned after a pipeline execution.
+        :return: dictionary of pandas dataframes, with the dataset names as the keys.
+        """
+
         # get results of interest
         filtered_results = AnalysisHelper.load_all_results({pipeline_name: [output_name]}, results)
 
@@ -61,6 +129,12 @@ class AnalysisHelper:
 
     @staticmethod
     def plot_compare_cumsum_deg_dist(results):
+        """Returns and plots the cumulative sum of degree distribution for all the datasets.
+
+        :param results: output format that is returned after a pipeline execution.
+        :return: dictionary of the cumulative degree distribution for all the datasets.
+        With the datasets names as the keys.
+        """
         from scipy import stats
 
         # get results of interest
@@ -83,17 +157,47 @@ class AnalysisHelper:
         return filtered_results
 
     @staticmethod
-    def get_common_nodes(pipeline_name, output_name, results):  # 'community_detection', 'nodes'
+    def get_common_nodes(results, pipeline_name='community_detection', output_name='nodes'):
+        """Returns and plots the shared nodes among multiple contexts.
+
+        It defaults on the nodes chosen with the community detection algorithm.
+
+        :param results: output format that is returned after a pipeline execution.
+        :param pipeline_name: name of the pipeline to which the desired output belongs to.
+        :param output_name: name of the output to which the desired output belongs to.
+        :return: pandas series with user_names as indexes and number of appearances as values.
+        """
+
         # get results of interest
         filtered_results = AnalysisHelper.get_single_summary(pipeline_name, output_name, results)
 
-        results = filtered_results.reset_index()['user_name'].value_counts().to_frame()
-        results = results[results['user_name'] > 1]
+        results = filtered_results.reset_index()['user_name'].value_counts().to_frame()\
+            .rename(columns={'user_name': 'no_participations'})
+        results = results[results['no_participations'] > 1]
+
+        # add user information
+        with db.session_scope() as session:
+            userinfo = pd.read_sql(session.query(User, Profile.follower_rank).join(Profile.user)
+                                   .filter(User.user_name.in_(results.index.tolist())).statement,
+                                   con=session.bind, index_col='user_name')
+
+        results = userinfo.merge(results, left_index=True, right_index=True)\
+            .drop(['id', 'following', 'followers', 'tweets', 'join_date'], axis=1)\
+            .sort_values(by='no_participations', ascending=False)
 
         return results
 
     @staticmethod
-    def plot_events_with_common_nodes(pipeline_name, output_name, results):  # 'community_detection', 'nodes'
+    def plot_events_with_common_nodes(results, pipeline_name='community_detection', output_name='nodes'):
+        """Returns and plots the shared nodes among multiple contexts.
+
+        It defaults on the nodes chosen with the community detection algorithm.
+
+        :param results: output format that is returned after a pipeline execution.
+        :param pipeline_name: name of the pipeline to which the desired output belongs to.
+        :param output_name: name of the output to which the desired output belongs to.
+        :return: pandas series with user_names as indexes and number of appearances as values.
+        """
         from matplotlib.ticker import MaxNLocator
 
         # get results of interest
@@ -113,16 +217,18 @@ class AnalysisHelper:
         # get total number of different users per event
         results = results.sum().to_frame().reset_index()
 
-        results.rename(columns={'index': 'event_name', 0: 'total'}, inplace=True)
+        results = results.rename(columns={'index': 'event_name', 0: 'total'}).sort_values(by='total', ascending=False)
 
         fig, ax = plt.subplots(figsize=(15, 8))
         sns.barplot(x='event_name', y='total', data=results)\
-            .set_title('Number of users per event that appear in more than one event')
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+            .set_title('Number of users per event that appear in multiple events')
+        for i, v in enumerate(results['total']):
+            ax.text(i, v + .2, str(v), ha='center')
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha='right')
         ax.yaxis.set_major_locator(MaxNLocator(integer=True))
         plt.xlabel('Events')
-        plt.ylabel('Number of users that appear in more than one event')
+        plt.ylabel('Number of users that appear in multiple events')
         plt.tight_layout()
         plt.show()
 
-        return results
+        return results.set_index('event_name')

71bd20db83df4fad8802695caed691ca8fb6f3e0
flaprimo <91EL(gi7hu8)barto91>
2019-02-14 14:10:52 +0000

    completed analysis helper refactoring

--- analysis_helper.py
+++ analysis_helper.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import func, desc, and_
 from datasources.database import User, Profile, Context, Graph, Partition, Community, UserCommunity, UserContext
 
 
@@ -74,3 +77,227 @@ class AnalysisHelper:
 
     def get_pquality(self):
         return self.__get_contexts_multiple('community_detection_metrics', 'pquality', 'pquality', 'csv')
+
+    def plot_cumsum_deg_dist(self):
+        from scipy.stats import zscore
+        cumsum_deg_dist = self.__get_contexts_multiple('network_metrics', 'cumsum_deg_dist', 'cumsum_deg_dist', 'csv')
+
+        fig, ax = plt.subplots(figsize=(15, 8))
+        for context_name, cdd in cumsum_deg_dist:
+            sns.lineplot(x=cdd['cumsum_of_the_no_of_nodes'], y=zscore(cdd.index), ax=ax, label=context_name)\
+                .set_title('Cumulative sum of degree distribution')
+
+        ax.axhline(0, ls='--')
+        plt.xlabel('Cumulative sum of degrees')
+        plt.ylabel('Number of nodes (normalized with z-score)')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+        return cumsum_deg_dist
+
+    def partition_stats(self):
+        partitions_summary = self.__get_contexts_multiple(
+            'community_detection_metrics', 'partition_summary', 'partition_summary', 'csv')
+        no_all_nodes = self.__get_contexts_single(
+            'network_metrics', 'graph_summary', 'graph_summary', 'csv')[['no_nodes']]\
+            .rename(columns={'no_nodes': 'no_all_nodes'})
+        no_cd_nodes = self.__get_contexts_single('network_creation', 'create_nodes', 'nodes', 'csv')[['user_name']] \
+            .reset_index().drop_duplicates().set_index('name').groupby('name').count() \
+            .rename(columns={'user_name': 'no_cd_nodes'})
+
+        # compute stats for each context
+        context_stats = no_all_nodes.merge(no_cd_nodes, left_index=True, right_index=True)
+
+        communities = pd.DataFrame([{
+            'name': context_name, 'no_communities': len(p.index), 'no_nodes_greatest_community': p.no_nodes.max()
+        } for context_name, p in partitions_summary]).set_index('name')
+
+        context_stats = context_stats.merge(communities, left_index=True, right_index=True)
+
+        context_stats['is_degenerate'] = \
+            context_stats.apply(lambda x: x['no_communities'] == 1 and
+                                         x['no_nodes_greatest_community'] == x['no_all_nodes'], axis=1)
+
+        # summarize each context
+        good_contexts = context_stats[~context_stats['is_degenerate']]
+
+        c_summary_dict = {
+            'degenerated_context_ratio': context_stats['is_degenerate'].sum() / len(context_stats.index),
+            'good_context_ratio': (~context_stats['is_degenerate']).sum() / len(context_stats.index),
+            'avg_communities_per_good_context': good_contexts['no_communities'].sum() / len(good_contexts.index),
+            'avg_sociable_users_ratio': (good_contexts['no_cd_nodes'] / good_contexts['no_all_nodes']).sum() / len(
+                good_contexts.index)
+        }
+        c_summary = pd.DataFrame(data=list(c_summary_dict.values()), index=list(c_summary_dict.keys()))\
+            .rename(columns={0: 'values'}).round(2)
+
+        return c_summary
+
+    def __get_shared_nodes(self, *file_args):
+        nodes = self.__get_contexts_single(*file_args).user_name.reset_index().drop_duplicates().user_name
+
+        # get shared nodes
+        shared_nodes = nodes.value_counts().where(lambda x: x > 1).dropna().astype(int).to_frame() \
+            .rename(columns={'user_name': 'no_participations'})
+
+        return shared_nodes
+
+    def compare_common_nodes(self):
+        # load nodes
+        all_shared_nodes = self.__get_shared_nodes('network_creation', 'create_nodes', 'nodes', 'csv')
+        cd_nodes = self.__get_shared_nodes('community_detection', 'add_communities_to_nodes', 'nodes', 'csv')
+
+        # check shared nodes survived
+        all_shared_nodes['is_present'] = all_shared_nodes.index.isin(cd_nodes.index)
+
+        return all_shared_nodes
+
+    def get_common_nodes(self):
+        # get shared nodes
+        shared_nodes = self.__get_shared_nodes(
+            'community_detection', 'add_communities_to_nodes', 'nodes', 'csv')
+
+        # add user information
+        with self.datasources.database.session_scope() as session:
+            userinfo = pd.read_sql(session.query(User, Profile.follower_rank).join(Profile.user)
+                                   .filter(User.user_name.in_(shared_nodes.index.tolist())).statement,
+                                   con=session.bind, index_col='user_name')
+
+        shared_nodes = userinfo.merge(shared_nodes, left_index=True, right_index=True) \
+            .drop(['id', 'following', 'followers', 'tweets', 'join_date'], axis=1) \
+            .sort_values(by='no_participations', ascending=False)
+
+        return shared_nodes
+
+    def plot_contexts_with_common_nodes(self):
+        from matplotlib.ticker import MaxNLocator
+
+        nodes = self.__get_contexts_single(
+            'community_detection', 'add_communities_to_nodes', 'nodes', 'csv').reset_index()[['user_name', 'name']]
+
+        # get dummies from event names
+        name_dummies = pd.get_dummies(nodes['name'])
+        node_participations = pd.concat([nodes['user_name'], name_dummies], axis=1)
+
+        # sum all events appearances by user
+        node_participations = node_participations.groupby('user_name').sum()
+
+        # keep events with > 1 appearance
+        node_participations = node_participations[node_participations.sum(axis=1) > 1]
+
+        # get total number of different users per context
+        node_participations = node_participations.sum().to_frame().reset_index()
+
+        node_participations = node_participations\
+            .rename(columns={'index': 'context_name', 0: 'total'}).sort_values(by='total', ascending=False)
+
+        fig, ax = plt.subplots(figsize=(15, 8))
+        sns.barplot(x='context_name', y='total', data=node_participations)\
+            .set_title('Number of users per event that appear in multiple contexts')
+        for i, v in enumerate(node_participations['total']):
+            ax.text(i, v + .2, str(v), ha='center')
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha='right')
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.xlabel('Events')
+        plt.ylabel('Number of users that appear in multiple contexts')
+        plt.tight_layout()
+        plt.show()
+
+        return node_participations.set_index('context_name')
+
+    def __get_active_users(self):
+        with self.datasources.database.session_scope() as session:
+            active_users = pd.read_sql(session.query(User.id, User.user_name, User.tweets)
+                                       .join(Profile)
+                                       .filter(Profile.follower_rank > 0).statement,
+                                       con=session.bind, index_col='id')
+
+            active_users['tweets'] = (active_users['tweets'] - active_users['tweets'].min()) /\
+                                     (active_users['tweets'].max() - active_users['tweets'].min())
+            active_users = active_users[active_users.tweets > 0.00005]
+
+        return active_users.index.tolist()
+
+    def rank_1(self):
+        active_users = self.__get_active_users()
+
+        with self.datasources.database.session_scope() as session:
+            userinfo = pd.read_sql(session.query(User.user_name, User.name, User.location,
+                                                 (func.ifnull(func.sum(1 / UserCommunity.indegree_centrality), 1) +
+                                                  func.ifnull(func.sum(UserContext.topical_focus), 0)).label('rank'))
+                                   .join(UserCommunity).join(UserContext)
+                                   .filter(User.id.in_(active_users))
+                                   .group_by(UserCommunity.user_id)
+                                   .order_by(desc('rank')).statement,
+                                   con=session.bind).round(decimals=3)
+
+        return userinfo
+
+    def rank_2(self):
+        active_users = self.__get_active_users()
+
+        def min_max(df):
+            min = df.min()
+            max = df.max()
+
+            return (df - min) / (max - min)
+
+        with self.datasources.database.session_scope() as session:
+            data = pd.read_sql(session.query(User.user_name,
+                                             Profile.follower_rank,
+                                             UserContext.topical_attachment,
+                                             UserCommunity.indegree_centrality)
+                               .join(Profile, User.id == Profile.user_id)
+                               .join(UserCommunity, Profile.user_id == UserCommunity.user_id)
+                               .join(Community, UserCommunity.community_id == Community.id)
+                               .join(Partition, Community.partition_id == Partition.id)
+                               .join(Graph, Partition.graph_id == Graph.id)
+                               .join(UserContext, and_(Graph.context_id == UserContext.context_id,
+                                     User.id == UserContext.user_id))
+                               .filter(User.id.in_(active_users)).statement,
+                               con=session.bind, index_col='user_name')
+
+        data['topical_attachment'] = min_max(data['topical_attachment'])
+
+        rank = data.groupby('user_name')\
+            .apply(lambda x: abs(x['follower_rank'].head(1) - 1) *
+                             (x['topical_attachment'].sum() + x['indegree_centrality'].sum()))\
+            .reset_index(level=0, drop=True).sort_values(ascending=False)\
+            .to_frame().rename(columns={'follower_rank': 'rank'})
+
+        return rank
+
+    def rank_3(self):
+        active_users = self.__get_active_users()
+
+        def min_max(df):
+            min = df.min()
+            max = df.max()
+
+            return (df - min) / (max - min)
+
+        with self.datasources.database.session_scope() as session:
+            data = pd.read_sql(session.query(User.user_name,
+                                             Profile.follower_rank,
+                                             UserContext.topical_attachment,
+                                             UserCommunity.indegree_centrality)
+                               .join(Profile, User.id == Profile.user_id)
+                               .join(UserCommunity, Profile.user_id == UserCommunity.user_id)
+                               .join(Community, UserCommunity.community_id == Community.id)
+                               .join(Partition, Community.partition_id == Partition.id)
+                               .join(Graph, Partition.graph_id == Graph.id)
+                               .join(UserContext, and_(Graph.context_id == UserContext.context_id,
+                                     User.id == UserContext.user_id))
+                               .filter(User.id.in_(active_users)).statement,
+                               con=session.bind, index_col='user_name')
+
+        data['topical_attachment'] = min_max(data['topical_attachment'])
+
+        rank = data.groupby('user_name')\
+            .apply(lambda x: abs(x['follower_rank'].head(1) - 1) *
+                             (x['topical_attachment'].sum() + 1 / (x['indegree_centrality'].sum() + 1)))\
+            .reset_index(level=0, drop=True).sort_values(ascending=False)\
+            .to_frame().rename(columns={'follower_rank': 'rank'})
+
+        return rank

426439dd99a01101b150d473a1bbb7d753dd46cd
flaprimo <91EL(gi7hu8)barto91>
2019-02-15 18:56:13 +0000

    changed default project name

--- twitter-network-analysis.py
+++ twitter-network-analysis.py
@@ -4,14 +4,13 @@ from orchestrator import Orchestrator
 PROJECT_PATH = os.path.abspath('')
 INPUT_PATH = os.path.join(PROJECT_PATH, 'input/')
 OUTPUT_PATH = os.path.join(PROJECT_PATH, 'output/')
-PROJECT_NAME = 'uk_healthcare_infomap_small'
+PROJECT_NAME = 'uk_healthcare_infomap'
 
 
 def main():
-
     o = Orchestrator(PROJECT_NAME, INPUT_PATH, OUTPUT_PATH)
     o.execute()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

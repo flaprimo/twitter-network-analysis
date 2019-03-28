a84711c5f1fda11d3e8f4284dd7a26624dd53e7a
flaprimo <91EL(gi7hu8)barto91>
2019-03-28 12:19:22 +0000

    added pager to user timelines retrieval, now tw api caches every request

--- datasources/tw/tw_api.py
+++ datasources/tw/tw_api.py
@@ -13,37 +13,37 @@ class TwApi:
                               auth_type='oAuth2')
         logger.debug('INIT Tw api')
 
-    @staticmethod
-    def __get_tweets(search, n):
+    def __get_tweets(self, pager, n, wait=5):
         tw_list = []
-        for i, tw_json in enumerate(search.get_iterator(), start=1):
-            if 'message' in tw_json:
-                logger.debug(f'{tw_json["message"]} ({tw_json["code"]})')
-            else:
-                tw_list.append(tw_json)
 
-            if i >= n:
-                break
+        with requests_cache.enabled(self.cache_path, expire_after=86400):
+            for i, tw in zip(range(n), pager.get_iterator(wait=wait)):
+                if 'message' in tw:
+                    logger.debug(f'{tw["message"]} ({tw["code"]})')
+                else:
+                    tw_list.append(tw)
 
         return tw_list
 
     def premium_search(self, product='fullarchive', label='prod', query='', since=None, until=None, n=100):
         logger.info(f'tw api search for: {query}')
 
-        search = TwitterPager(self.api, f'tweets/search/{product}/:{label}',
-                              {'query': query,
-                               'fromDate': since.strftime('%Y%m%d%H%M'),
-                               'toDate': until.strftime('%Y%m%d%H%M')})
+        pager = TwitterPager(self.api, f'tweets/search/{product}/:{label}',
+                             {'query': query,
+                              'fromDate': since.strftime('%Y%m%d%H%M'),
+                              'toDate': until.strftime('%Y%m%d%H%M')})
 
-        return self.__get_tweets(search, n)
+        return self.__get_tweets(pager, n)
 
     def get_user_timeline(self, user_name, n=200):
         logger.info(f'tw api timeline for user: {user_name}')
 
-        with requests_cache.enabled(self.cache_path, expire_after=86400):
-            user_timeline = self.api.request('statuses/user_timeline',
-                                             {'screen_name': user_name, 'count': n, 'exclude_replies': 'true'})
-        return user_timeline
+        pager = TwitterPager(self.api, 'statuses/user_timeline',
+                             {'screen_name': user_name,
+                              'count': n,
+                              'exclude_replies': 'true'})
+
+        return self.__get_tweets(pager, n, 2)
 
     def get_user_timelines(self, user_name_list, n=200):
         logger.info(f'tw api timeline for {len(user_name_list)} users')
@@ -51,9 +51,8 @@ class TwApi:
         stream = []
         for u in user_name_list:
             user_stream = self.get_user_timeline(u, n)
-            user_stream_json = user_stream.json()
-            if isinstance(user_stream_json, list):  # filter out user timelines with errors
-                stream.append({'user_name': u, 'stream': user_stream.json()})
+            if user_stream:
+                stream.append({'user_name': u, 'stream': user_stream})
             time.sleep(2)
 
         return stream
@@ -62,18 +61,18 @@ class TwApi:
         logger.info(f'tw api profiles for {len(user_name_list)} users')
 
         # group usernames in 100 lists
-        user_groups = [user_name_list[n:n + 100] for n in range(0, len(user_name_list), 100)]
+        u_groups = [user_name_list[n:n + 100] for n in range(0, len(user_name_list), 100)]
 
         stream = []
-        for u_list in user_groups:
+        for u_list in u_groups:
             with requests_cache.enabled(self.cache_path, expire_after=86400):
-                user_stream = self.api.request('users/lookup', {'screen_name': u_list, 'include_entities': 'false'})
-            stream.extend(user_stream)
+                u_stream = self.api.request('users/lookup', {'screen_name': u_list, 'include_entities': 'false'})
+            stream.extend(u_stream)
             time.sleep(3)
 
         return stream
 
     def get_rate_limit_status(self, resources):
-        rate_limit_status = self.api.request('application/rate_limit_status', {'resources': resources})
+        rate_limit_status = self.api.request('application/rate_limit_status', {'resources': resources}).json()
 
         return rate_limit_status
